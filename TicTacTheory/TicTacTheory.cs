ab9d7f9ed880085c4c4827e0046eaa5078159b63
FabioBis <fabio.biselli.80@gmail.com>
2014-05-17 15:37:43 +0200

    Added auxiliary enums.

--- TicTacTheory/TicTacTheory.cs
+++ TicTacTheory/TicTacTheory.cs
@@ -9,6 +9,47 @@ using System.Windows.Forms;
 
 namespace TicTacTheory
 {
+    /// <summary>
+    /// Defines the type of opponent for the single game.
+    /// </summary>
+    enum Opponent
+    {
+        // Player VS Player.
+        Manual,
+        // Player VS AI.
+        Sheldon,
+        Penny,
+        Stuart,
+        // Not yet defined.
+        Empty
+    }
+
+    /// <summary>
+    /// Defines the first to move for the single game.
+    /// </summary>
+    enum First
+    {
+        // The player.
+        Me,
+        // The AI.
+        Opponent,
+        // Not yet defined.
+        Empty
+    }
+
+    /// <summary>
+    /// Defines AI agents for the game streak test.
+    /// </summary>
+    enum AI
+    {
+        // Defined AI.
+        Sheldon,
+        Penny,
+        Stuart,
+        // Not yet defined.
+        Empty
+    }
+
     public partial class TicTacTheory : Form
     {
         public TicTacTheory()
